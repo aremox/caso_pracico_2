17bfa87300e8784bab8cdee4cbd72bee9042a863
Iván Arenas Morante <arenasmorante@gmail.com>
2022-07-10 20:18:15 +0200

    Entrega final

---
+++ caso2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 
+# AUTOR: Iván Arenas Morante
+# TITULO: Caso practico 2 
+# DESCRIPCIÓN:
+# Script para lanzar terraform + ansible de manera conjunta 
+# 
+cd /etc/terraform/k8s/
+terraform apply -auto-approve && ansible-playbook -i /etc/ansible/inventory /etc/ansible/playbook/all.yml
+
+exit $?
